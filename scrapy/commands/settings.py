ebbc2f05bf49bb68934cbcd66a90442922876798
rutgerbv <frankenrutger@gmail.com>
2024-06-17 03:18:36 +0200

    added coverage in command/settings.py for run function in class Command.

--- scrapy/commands/settings.py
+++ scrapy/commands/settings.py
@@ -5,6 +5,24 @@ from typing import List
 from scrapy.commands import ScrapyCommand
 from scrapy.settings import BaseSettings
 
+branch_coverage_run = {
+    "run_1": False,
+    "run_1-1": False,
+    "run_1-2": False,
+    "run_2": False,
+    "run_3": False,
+    "run_4": False,
+    "run_5": False,
+    "run_default": False
+}
+
+
+def _print_branch_coverage():
+    coveragePercentage = (sum(branch_coverage_run.values()) / len(branch_coverage_run)) * 100
+    for branch, executed in branch_coverage_run.items():
+        print(f"{branch} has been {'executed' if executed else 'missed'}")
+    print(f"Branch coverage: {coveragePercentage}%")
+
 
 class Command(ScrapyCommand):
     requires_project = False
@@ -50,16 +68,26 @@ class Command(ScrapyCommand):
         assert self.crawler_process
         settings = self.crawler_process.settings
         if opts.get:
+            branch_coverage_run["run_1"] = True
             s = settings.get(opts.get)
             if isinstance(s, BaseSettings):
+                branch_coverage_run["run_1-1"] = True
                 print(json.dumps(s.copy_to_dict()))
             else:
+                branch_coverage_run["run_1-2"] = True
                 print(s)
         elif opts.getbool:
+            branch_coverage_run["run_2"] = True
             print(settings.getbool(opts.getbool))
         elif opts.getint:
+            branch_coverage_run["run_3"] = True
             print(settings.getint(opts.getint))
         elif opts.getfloat:
+            branch_coverage_run["run_4"] = True
             print(settings.getfloat(opts.getfloat))
         elif opts.getlist:
+            branch_coverage_run["run_5"] = True
             print(settings.getlist(opts.getlist))
+        else:
+            branch_coverage_run["run_default"] = True
+
---
+++ scrapy/commands/test_run_function.py
@@ -0,0 +1,1 @@
+from settings import Command, branch_coverage_run, _print_branch_coverage
