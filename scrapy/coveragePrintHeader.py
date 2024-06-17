277bfc4a1b91e74f40326a476947e2e18b5caf5f
rutgerbv <frankenrutger@gmail.com>
2024-06-17 18:36:19 +0200

    added .for import

--- scrapy/coveragePrintHeader.py
+++ scrapy/coveragePrintHeader.py
@@ -1,6 +1,14 @@
-from scrapy.cmdline import _print_branch_coverage, _print_header
-from scrapy.settings import BaseSettings
-
-settings = BaseSettings()
-_print_header(settings, True)
-_print_branch_coverage()
+# from scrapy.cmdline import _print_branch_coverage, _print_header
+# from scrapy.http.request import Request
+# from scrapy.settings import BaseSettings
+#
+#
+# def coveragePrintHeader():
+#     settings = {'TEST_BOT': 'coverage'}
+#     _print_header(settings, False)
+#     _print_branch_coverage()
+#     _print_header(settings, True)
+#     _print_branch_coverage()
+#
+# if __name__ == "__main__":
+#     coveragePrintHeader()
